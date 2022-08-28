285af7d0b1bd6307dfe9f2e81b81c1cb3fb678b7
joelsleeba <joelsleeba1@gmail.com>
2022-08-28 10:58:37 +0530

    SVG paths added

---
+++ ToDo.md
@@ -0,0 +1,4 @@
+- [ ] Upload images and fonts to uploads.igem.org and link them directly to the source code
+- [ ] After uploading change the links at
+    1. line 11565, static/index.css
+    2. line 28, static/bottle.css
---
+++ static/bottle.js
@@ -0,0 +1,81 @@
+var textContainer0 = document.querySelector('#bottle-path-container');
+var textContainer1 = document.querySelector('#bottle-path-container1');
+const topElement = document.getElementById('header');
+const bottomElement = document.getElementById('end-index');
+
+
+//sets path height, by updating the html code
+function setPathHeight() {
+    var pathTopY = topElement.getBoundingClientRect().bottom;
+    var pathBottomY = bottomElement.getBoundingClientRect().top;
+
+    textContainer0.style['height'] = `${pathBottomY-pathTopY}px`;
+    textContainer1.style['height'] = `${pathBottomY-pathTopY}px`;
+    console.log(pathBottomY-pathTopY);
+
+}
+
+setPathHeight();
+
+var textPath0 = document.getElementById('bottle');
+var textPath1 = document.getElementById('bottle1');
+
+var path0 = document.querySelector( textPath0.getAttribute('href') );
+var path1 = document.querySelector( textPath1.getAttribute('href') );
+
+var pathLength = path0.getTotalLength();
+
+function updateTextPathOffset(offset){
+  textPath0.setAttribute('startOffset', offset);
+  textPath1.setAttribute('startOffset', offset);
+}
+
+updateTextPathOffset(0);
+
+function bottleScroll(){
+  requestAnimationFrame(function(){
+  
+    var rect = textContainer0.getBoundingClientRect();
+    var scrollOffset = (window.innerHeight/2 - (rect.y));
+    
+    if (scrollOffset>=0){
+        var scrollPercent = (scrollOffset)/(rect.height);
+        updateTextPathOffset(pathLength*scrollPercent);
+    }
+  });
+}
+
+window.addEventListener('load', setPathHeight);
+window.addEventListener('resize', setPathHeight);
+window.addEventListener('scroll',bottleScroll);
+
+//animations
+const fadeInObs = document.querySelectorAll('.fadeInObs');
+const leftSlideObs = document.querySelectorAll('.leftSlideObs');
+const rightSlideObs = document.querySelectorAll('.rightSlideObs');
+const topSlideObs = document.querySelectorAll('.topSlideObs');
+const bottomSlideObs = document.querySelectorAll('.bottomSlideObs');
+
+const FIobserver = new IntersectionObserver((entries, FIobserver)=>{
+    entries.forEach(entry =>{
+        console.log(entry.target);
+        if(!entry.isIntersecting) return;
+        entry.target.classList.add("animateObs");
+    });
+}, {root: null, threshold:0, rootMargin: "0px 0px -150px 0px"});
+
+fadeInObs.forEach(obj => {
+    FIobserver.observe(obj);
+});
+leftSlideObs.forEach(obj => {
+    FIobserver.observe(obj);
+});
+rightSlideObs.forEach(obj => {
+    FIobserver.observe(obj);
+});
+bottomSlideObs.forEach(obj => {
+    FIobserver.observe(obj);
+});
+topSlideObs.forEach(obj => {
+    FIobserver.observe(obj);
+});
